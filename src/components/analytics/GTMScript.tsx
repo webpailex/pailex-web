fdc430cfbfa78e8b1f237e6160e5f100d7c1d0d9
William0848 <william.cuellar@futurite.com>
2026-09-17 12:04:37 -0600

    cambios 17/09/26

--- src/components/analytics/GTMScript.tsx
+++ src/components/analytics/GTMScript.tsx
@@ -3,7 +3,7 @@ import Script from "next/script";
 /** Contenedor principal de tracking: ver arquitectura en TrackingSettings del gestor. */
 export function GTMScript({ containerId }: { containerId: string }) {
   return (
-    <Script id="gtm-script" strategy="afterInteractive">
+    <Script id="gtm-script" strategy="beforeInteractive">
       {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${containerId}');`}
     </Script>
   );
